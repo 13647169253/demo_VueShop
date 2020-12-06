bc68e6ffc503925c7d38d4c88fe8c270f6f74582
Yaxiang <liuyaxiang123@icloud.com>
2020-12-06 11:11:35 +0800

    Off

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import addGoods from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 
 Vue.use(VueRouter)
@@ -31,6 +32,7 @@ const router = new VueRouter({
         { path: '/goods', component: GoodsList },
         { path: '/goods/add', component: addGoods },
         { path: '/orders', component: Order },
+        { path: '/reports', component: Report },
       ]
     },
   ]
